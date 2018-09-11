f798cfaec72873ef9d2dfdfa1fe8f5af2c7d1b25
HanXiao <Yipartner@1350690718@qq.com>
2018-09-11 17:20:44 +0800

    作业接口认证设计

--- database/migrations/2018_09_11_084540_create_class_table.php
+++ database/migrations/2018_09_11_084540_create_class_table.php
@@ -22,6 +22,9 @@ class CreateClassTable extends Migration
             $table->integer('fee');
             //最大押金退还额度
             $table->float('fee_back_num');
+            //用于作业接口认证
+            $table->string('access_token')->nullable();
+            $table->string('access_secret')->nullable();
             $table->timestamps();
         });
     }
