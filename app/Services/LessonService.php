7879dd894ec93798931aeb830faecda7de44bfe9
liuyonghui <1359158019@qq.com>
2018-09-12 21:23:55 +0800

    完成课时Service

---
+++ app/Services/LessonService.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lyh
+ * Date: 18/9/11
+ * Time: 下午7:37
+ */
+
+namespace App\Services;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class LessonService
+{
+    private $tbName = 'lessons';
+
+    public function createLesson($teacherId, $lessonInfo)
+    {
+        $time = new Carbon();
+        $fullLessonInfo = array_merge($lessonInfo, [
+            'teacher_id' => $teacherId,
+            'created_at' => $time,
+            'updated_at' => $time
+        ]);
+        $lessonId = DB::table($this->tbName)->insertGetId($fullLessonInfo);
+        return $lessonId;
+    }
+
+    public function updateLesson($lessonId, $lessonInfo)
+    {
+        $time = new Carbon();
+        $lessonInfo['updated_at'] = $time;
+        DB::table($this->tbName)->where('id', $lessonId)->update($lessonInfo);
+    }
+
+    public function getLessonInfoById($lessonId)
+    {
+        $lessonInfo = DB::table($this->tbName)->where('id', $lessonId)->first();
+        return $lessonInfo;
+    }
+
+    public function getLessonListByClassId($classId)
+    {
+        $lessonesInfo = DB::table($this->tbName)->where('class_id', $classId)->get();
+        return $lessonesInfo;
+    }
+
+    public function deleteLesson($lessonId)
+    {
+        DB::table($this->tbName)->where('id', $lessonId)->delete();
+    }
+
+    public function updateLessionMovie($lessonId, $movieId)
+    {
+        DB::table($this->tbName)->where('id', $lessonId)->update([
+            'movie_id' => $movieId
+        ]);
+    }
+}
