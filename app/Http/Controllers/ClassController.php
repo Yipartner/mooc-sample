828f1845619f73aa95c943bdd1729464cdc7172a
liuyonghui <1359158019@qq.com>
2018-09-13 22:31:24 +0800

    添加创建课程

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ClassService;
+use App\Tools\Code;
+use App\Tools\ValidationHelper;
 use Illuminate\Http\Request;
 
 class ClassController extends Controller
 {
-    //
+    private $classService;
+
+    public function __construct(ClassService $classService)
+    {
+        $this->classService = $classService;
+    }
+
+
+    public function createClass(Request $request)
+    {
+        $userInfo = $request->user;
+        if(!$userInfo->is_teacher)
+            return response()->json(Code::NO_PERMISSION);
+        $classRule  = [
+            'name' => 'required',
+            'content' => 'required',
+            'fee' => 'required',
+            'fee_back_num' => 'required'
+        ];
+        $classInfo = ValidationHelper::getInputData($request, $classRule);
+        $classId = $this->classService->createClass($userInfo->id,$classInfo);
+        return response()->json([
+            'code' => 0,
+            'message' => 'create class Success',
+            'class_id' => $classId
+        ]);
+    }
 }
--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\LessonService;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class LessonController extends Controller
 {
+    private $lessonService;
+    private $userService;
+
+
+    public function __construct(LessonService $lessonService,UserService $userService)
+    {
+        $this->lessonService = $lessonService;
+        $this->userService = $userService;
+    }
+
+
 
 }
